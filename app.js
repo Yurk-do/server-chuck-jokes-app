f01d8e69198aded60859df5687def8ec101cfe9b
Yurk-do <yurk-do@yandex.ru>
2021-10-24 22:53:00 +0300

    change host

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 const http = require('http').createServer(app);
